52dde08fade674b9913614c1f4405602c939637f
Mdbshero <mdbshero@gmail.com>
2022-03-30 19:48:45 -0500

    Added GET requests and POST request.

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
 //imports
 const express = require("express");
 const app = express();
-
+const repoContext = require("./repository/repository-wrapper")
 
 //Middleware
 app.use(express.json());
+app.use(express.urlencoded({extended: true}))
 
+// Endpoints
+// http://localhost:5005 (BASE URL)
 
+// GET all products
+// http://localhost:5005/api/products
+app.get("/api/products", (req, res) => {
+    const products = repoContext.products.findAllProducts();
+    return res.send(products);
+});
+
+// GET product by id
+// http://localhost:5005/api/products/:id
+app.get("/api/products/:id", (req, res) => {
+    const id = req.params.id;
+    const product = repoContext.products.findProductById(id);
+    return res.send(product);
+})
+
+//POST new product
+// http://locathost:5005/api/products
+app.post("/api/products", (req, res) => {
+    const newProduct = req.body;
+    const addedProduct = repoContext.products.createProduct(newProduct)
+    return res.status(201).send(addedProduct)
+})
 // Starting a Server
 const PORT = process.env.PORT || 5005;
 
 app.listen(PORT, () => {
-    console.log(`Server running, student! On PORT: ${PORT}`);
+  console.log(`Server running, student! On PORT: ${PORT}`);
 });
